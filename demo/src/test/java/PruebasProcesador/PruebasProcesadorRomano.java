b2d17837dc81a6f777495902d59182ba7cc472f7
betosr20 <betog19@gmail.com>
2018-02-22 12:58:07 -0600

    Se procede a realizar un refactor del codigo de pruebas para evitar tener tantos new en cada prueba

--- demo/src/test/java/PruebasProcesador/PruebasProcesadorRomano.java
+++ demo/src/test/java/PruebasProcesador/PruebasProcesadorRomano.java
@@ -5,28 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PruebasProcesadorRomano {
-
+	private ProcesadorRomano procesador = new ProcesadorRomano();
 	@Test
 	public void test1() throws Exception {
-		ProcesadorRomano procesador = new ProcesadorRomano();
 		assertEquals(0, procesador.Convierte(""));
 		
 	}
 	@Test
 	public void test2() throws Exception {
 		boolean excepcion=false;
-		ProcesadorRomano procesador2 = new ProcesadorRomano();
-		assertEquals(1, procesador2.Convierte("I"));
-		assertEquals(2, procesador2.Convierte("II"));
-		assertEquals(3, procesador2.Convierte("III"));
-		assertEquals(5, procesador2.Convierte("V"));
-		assertEquals(10, procesador2.Convierte("X"));
-		assertEquals(50, procesador2.Convierte("L"));
-		assertEquals(100, procesador2.Convierte("C"));
-		assertEquals(500, procesador2.Convierte("D"));
-		assertEquals(1000, procesador2.Convierte("M"));
+		assertEquals(1, procesador.Convierte("I"));
+		assertEquals(2, procesador.Convierte("II"));
+		assertEquals(3, procesador.Convierte("III"));
+		assertEquals(5, procesador.Convierte("V"));
+		assertEquals(10, procesador.Convierte("X"));
+		assertEquals(50, procesador.Convierte("L"));
+		assertEquals(100, procesador.Convierte("C"));
+		assertEquals(500, procesador.Convierte("D"));
+		assertEquals(1000, procesador.Convierte("M"));
 		try {
-		assertEquals(1000, procesador2.Convierte("IIII"));
+		assertEquals(1000, procesador.Convierte("IIII"));
 		}catch (Exception e){
 			excepcion = true;
 		}
@@ -35,7 +33,6 @@ public class PruebasProcesadorRomano {
 
 	@Test
 	public void test3() throws Exception {
-		ProcesadorRomano procesador3 = new ProcesadorRomano();
 		boolean excepcionI =false;
 		boolean excepcionL =false;
 		boolean excepcionD =false;
@@ -43,29 +40,29 @@ public class PruebasProcesadorRomano {
 		boolean excepcionC =false;
 		
 		try {
-		assertEquals(3, procesador3.Convierte("IIIII"));
+		assertEquals(3, procesador.Convierte("IIIII"));
 		}catch (Exception e) {
 			excepcionI = true;
 		}
 		try {
-		assertEquals(3, procesador3.Convierte("LL"));
+		assertEquals(3, procesador.Convierte("LL"));
 		}catch (Exception e) {
 			excepcionL = true;
 		}
 		try {
-		assertEquals(3, procesador3.Convierte("DD"));
+		assertEquals(3, procesador.Convierte("DD"));
 		}catch (Exception e) {
 			excepcionD = true;
 		}
 		
 		try {
-		assertEquals(3, procesador3.Convierte("XXXX"));
+		assertEquals(3, procesador.Convierte("XXXX"));
 		}catch (Exception e) {
 			excepcionX = true;
 		}
 		
 		try {
-		assertEquals(3, procesador3.Convierte("CCCC"));
+		assertEquals(3, procesador.Convierte("CCCC"));
 		}catch (Exception e) {
 			excepcionC = true;
 		}
@@ -80,9 +77,8 @@ public class PruebasProcesadorRomano {
 	@Test
 	public void test4() throws Exception {
 		boolean excepcion=false;
-		ProcesadorRomano procesador4 = new ProcesadorRomano();
 		try {
-		assertEquals(1000, procesador4.Convierte("XXL"));
+		assertEquals(1000, procesador.Convierte("XXL"));
 		}catch (Exception e){
 			excepcion = true;
 		}
@@ -91,13 +87,11 @@ public class PruebasProcesadorRomano {
 
 	@Test
 	public void test5() throws Exception {
-		ProcesadorRomano procesador5 = new ProcesadorRomano();
-		assertEquals(60, procesador5.Convierte("LX"));
-		assertEquals(16, procesador5.Convierte("XVI"));
-		assertEquals(8, procesador5.Convierte("VIII"));
-		assertEquals(21, procesador5.Convierte("XXI"));
-		assertEquals(101, procesador5.Convierte("CI"));
-		assertEquals(161, procesador5.Convierte("CLXI"));
-	
+		assertEquals(60, procesador.Convierte("LX"));
+		assertEquals(16, procesador.Convierte("XVI"));
+		assertEquals(8, procesador.Convierte("VIII"));
+		assertEquals(21, procesador.Convierte("XXI"));
+		assertEquals(101, procesador.Convierte("CI"));
+		assertEquals(161, procesador.Convierte("CLXI"));
 	}
 }
