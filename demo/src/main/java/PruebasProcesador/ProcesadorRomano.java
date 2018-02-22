981060de022eeebf2283117f2015dfe63b13254d
betosr20 <betog19@gmail.com>
2018-02-21 19:11:31 -0600

    Se crea la prueba 1

---
+++ demo/src/main/java/PruebasProcesador/ProcesadorRomano.java
@@ -0,0 +1,5 @@
+package PruebasProcesador;
+
+public class ProcesadorRomano {
+
+}
---
+++ demo/src/test/java/PruebasProcesador/PruebasProcesadorRomano.java
@@ -0,0 +1,16 @@
+package PruebasProcesador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebasProcesadorRomano {
+
+	@Test
+	public void test1() {
+		ProcesadorRomano procesador = new ProcesadorRomano();
+		assertEquals(procesadorRomano.Convierte(""), 0);
+		
+	}
+
+}
