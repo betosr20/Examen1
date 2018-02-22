74d3e72ee57c9cc0a4d1e09846e7bd288ec932aa
betosr20 <betog19@gmail.com>
2018-02-21 19:30:59 -0600

    Se programa la prueba numero 2

--- demo/src/test/java/PruebasProcesador/PruebasProcesadorRomano.java
+++ demo/src/test/java/PruebasProcesador/PruebasProcesadorRomano.java
@@ -12,5 +12,17 @@ public class PruebasProcesadorRomano {
 		assertEquals(procesador.Convierte(" "), 0);
 		
 	}
-
+	@Test
+	public void test2() {
+		ProcesadorRomano procesador2 = new ProcesadorRomano();
+		assertEquals(procesador2.Convierte("I"), 1);
+		assertEquals(procesador2.Convierte("II"), 2);
+		assertEquals(procesador2.Convierte("III"), 3);
+		assertEquals(procesador2.Convierte("V"), 5);
+		assertEquals(procesador2.Convierte("X"), 10);
+		assertEquals(procesador2.Convierte("L"), 50);
+		assertEquals(procesador2.Convierte("C"), 100);
+		assertEquals(procesador2.Convierte("D"), 500);
+		assertEquals(procesador2.Convierte("M"), 1000);
+	}
 }
